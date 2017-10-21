b7a79a8c52b94281fb0c9c960afddaebf2cc366f
Raymond Aranda <raymond.aranda@gmail.com>
2017-10-21 01:02:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,9 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  index.between?(0, 8) && !position_taken?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  end
 end
 
 def position_taken?(board, index)
