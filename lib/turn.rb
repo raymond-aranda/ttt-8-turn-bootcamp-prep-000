f39830245f250261c555047bc2a63c0cf6a9517c
Raymond Aranda <raymond.aranda@gmail.com>
2017-10-21 00:39:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,4 +25,11 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  get input
+  #convert input to index
+  #if index is valid
+  #  make the move for input
+  #else
+  #  ask for input again until you get a valid input
+  #end
 end
